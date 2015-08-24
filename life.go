004f68d03499c11c20b89f90feea61ddf05a9909
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2015-08-24 20:53:43 +0100

    Board access

--- life.go
+++ life.go
@@ -1,5 +1,42 @@
 package main
 
+type Board struct {
+	cells [][]bool
+}
+
+func MakeBoard(rows, cols int) *Board {
+	all := make([]bool, rows*cols)
+	c := make([][]bool, rows)
+	for i := range c {
+		c[i] = all[i*cols : (i+1)*cols]
+	}
+	return &Board{cells: c}
+}
+
+func (b *Board) Rows() int {
+	return len(b.cells)
+}
+
+func (b *Board) Cols() int {
+	return len(b.cells[0])
+}
+
+func (b *Board) Set(r, c int, v bool) {
+	r, c = b.wrap(r, c)
+	b.cells[r][c] = v
+}
+
+func (b *Board) Get(r, c int) bool {
+	r, c = b.wrap(r, c)
+	return b.cells[r][c]
+}
+
+func (b *Board) wrap(r, c int) (int, int) {
+	r = (r + b.Rows()) % b.Rows()
+	c = (c + b.Cols()) % b.Cols()
+	return r, c
+}
+
 func main() {
 
 }
--- life_test.go
+++ life_test.go
@@ -3,5 +3,44 @@ package main
 import "testing"
 
 func TestMakeBoard(t *testing.T) {
+	b := MakeBoard(5, 3)
+	if b.Rows() != 5 || b.Cols() != 3 {
+		t.Fail()
+	}
+}
 
+func TestBoardAccess(t *testing.T) {
+	b := MakeBoard(3, 4)
+	b.Set(0, 0, true)
+	b.Set(2, 3, true)
+	b.Set(1, 1, true)
+
+	test := []struct {
+		r, c int
+		want bool
+	}{
+		{0, 0, true},
+		{0, 1, false},
+		{0, 2, false},
+		{0, 3, false},
+		{1, 0, false},
+		{1, 1, true},
+		{1, 2, false},
+		{1, 3, false},
+		{2, 0, false},
+		{2, 1, false},
+		{2, 2, false},
+		{2, 3, true},
+
+		{0, 4, true},
+		{-1, 0, false},
+		{0, -1, false},
+		{3, 0, true},
+	}
+
+	for _, c := range test {
+		if have := b.Get(c.r, c.c); have != c.want {
+			t.Errorf("%#v: got: %v", c, have)
+		}
+	}
 }
