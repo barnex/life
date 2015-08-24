78c7390d6f468f058c33016d3c1a5d47149fb497
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2015-08-24 20:54:26 +0100

    cleanup

--- life.go
+++ life.go
@@ -4,23 +4,6 @@ type Board struct {
 	cells [][]bool
 }
 
-func MakeBoard(rows, cols int) *Board {
-	all := make([]bool, rows*cols)
-	c := make([][]bool, rows)
-	for i := range c {
-		c[i] = all[i*cols : (i+1)*cols]
-	}
-	return &Board{cells: c}
-}
-
-func (b *Board) Rows() int {
-	return len(b.cells)
-}
-
-func (b *Board) Cols() int {
-	return len(b.cells[0])
-}
-
 func (b *Board) Set(r, c int, v bool) {
 	r, c = b.wrap(r, c)
 	b.cells[r][c] = v
@@ -29,6 +12,22 @@ func (b *Board) Set(r, c int, v bool) {
 func (b *Board) Get(r, c int) bool {
 	r, c = b.wrap(r, c)
 	return b.cells[r][c]
+}
+func (b *Board) Rows() int {
+	return len(b.cells)
+}
+
+func (b *Board) Cols() int {
+	return len(b.cells[0])
+}
+
+func MakeBoard(rows, cols int) *Board {
+	all := make([]bool, rows*cols)
+	c := make([][]bool, rows)
+	for i := range c {
+		c[i] = all[i*cols : (i+1)*cols]
+	}
+	return &Board{cells: c}
 }
 
 func (b *Board) wrap(r, c int) (int, int) {
