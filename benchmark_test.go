81a9991f1ebf8e63e7c371fd9b474550d873b0d0
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2015-09-20 21:26:32 +0100

    more benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -2,11 +2,14 @@ package life
 
 import "testing"
 
-func Benchmark64(b *testing.B)  { benchmark(b, 8) }
-func Benchmark1k(b *testing.B)  { benchmark(b, 32) }
-func Benchmark1M(b *testing.B)  { benchmark(b, 1024) }
-func Benchmark16M(b *testing.B) { benchmark(b, 4096) }
-func Benchmark64M(b *testing.B) { benchmark(b, 8192) }
+func Benchmark64(b *testing.B)   { benchmark(b, 8) }
+func Benchmark1k(b *testing.B)   { benchmark(b, 32) }
+func Benchmark4k(b *testing.B)   { benchmark(b, 64) }
+func Benchmark64k(b *testing.B)  { benchmark(b, 265) }
+func Benchmark1M(b *testing.B)   { benchmark(b, 1024) }
+func Benchmark16M(b *testing.B)  { benchmark(b, 4096) }
+func Benchmark64M(b *testing.B)  { benchmark(b, 8192) }
+func Benchmark256M(b *testing.B) { benchmark(b, 16384) }
 
 func benchmark(b *testing.B, N int) {
 	w := MakeBoard(N, N)
